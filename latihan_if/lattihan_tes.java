a8e6720b8b6a6ff89f248843248770ff14b01a00
Zein-praft <zeinfahmi449@gmail.com>
2024-08-22 06:50:37 +0700

    Add files via upload

---
+++ latihan_if/lattihan_tes.java
@@ -0,0 +1,14 @@
+package latihan_if;
+
+public class lattihan_tes {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 12;
+    // 0 OR 1
+    if ((a > b) || (a < b)) {
+      System.out.println("TRUE");
+    } else {
+      System.out.println("FALSE");
+    }
+  }
+}
